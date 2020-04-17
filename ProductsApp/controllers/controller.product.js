1008d530596b4cebdc6094180621b47599d02649
punesh12 <30601775+punesh12@users.noreply.github.com>
2020-04-17 17:07:29 +0530

    Add files via upload

---
+++ ProductsApp/controllers/controller.product.js
@@ -0,0 +1,23 @@
+var Product= require('../models/product.model.js');
+
+//Simple version, without validation or sanitation
+
+exports.test= function(req,res){
+    res.send('Greetings from the Test controller');
+};
+
+exports.product_create = function (req, res) {
+    var product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
